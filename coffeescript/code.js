b78410444054289d357a22db96d48190c0b97241
unknown <Slava@Terminal.(none)>
2012-03-20 22:09:23 +0400

    start lectures

--- coffeescript/code.js
+++ coffeescript/code.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 		var html = $(this).text();
 		//html = html.replace(/('|")([\w\s]*?)('|")/igm, '<span class="string">\'$2\'</span>')
 
-		html = html.replace(/(new|function|return|super|for|var|\sextends|class|@\w+|->|this)/igm, '<span class="rule">$1</span>');
+		html = html.replace(/(new|function|return|super|for|var|\sextends|class|@\w+|->|this|await|defer|autocb)/igm, '<span class="rule">$1</span>');
 		$(this).html(html);
 	});
 });
